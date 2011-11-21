3fa2081c1cc98230771969b16339d89acc51cb09
Jair Gaxiola <jyr.gaxiola@gmail.com>
2011-11-20 21:14:40 -0600

    tableview

---
+++ ios/NavTab02/NavTab/NavTab/Tab1_GreenScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  Tab1_GreenScreenViewController.h
+//  NavTab
+//
+//  Created by Jair Gaxiola on 11/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Tab1_GreenScreenViewController : UIViewController
+
+@end
---
+++ ios/NavTab02/NavTab/NavTab/Tab2_ItemViewController.m
@@ -0,0 +1,60 @@
+//
+//  Tab2_ItemViewController.m
+//  NavTab
+//
+//  Created by Jair Gaxiola on 11/20/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Tab2_ItemViewController.h"
+
+@implementation Tab2_ItemViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+/*
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView
+{
+}
+*/
+
+/*
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+*/
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
