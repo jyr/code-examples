5fccbc7a98fc35640b958f11b3fb3e8bbcc92f2e
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2014-01-14 01:56:07 -0600

    hello world and numbers

---
+++ Go/gobyexample/values.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("go" + "lang")
+  fmt.Println("1+1=", 1+1)
+  fmt.Println
+}
---
+++ Go/golang_book/chapter03/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("1 + 1 = ", 1+1)
+  fmt.Println("1 + 1 = ", 1.0+1)
+  fmt.Println("1 / 1 = ", 1.0/1.1)
+  fmt.Println("1 * 1 = ", 1*1)
+}
