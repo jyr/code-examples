9bcbd5467131dd9519a3836f7e277620528481e7
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2014-01-14 21:41:12 -0600

    variables, constants, scope

---
+++ Go/golang_book/chapter04/string01.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+  var x string
+  x = "first"
+  fmt.Println(x)
+  x = x + " second"
+  fmt.Println(x)
+
+  var z string = "Hello"
+  var y string = "World"
+  fmt.Println(z == y)
+
+  h := "Hello Word"
+  fmt.Println(h)
+
+  i := 5
+  fmt.Println(i)
+}
